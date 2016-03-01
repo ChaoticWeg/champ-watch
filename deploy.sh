28e0e5aac7fe4fa1c69295d85a1b6b7d3a2bb438
ChaoticWeg <shawn.lutch94@gmail.com>
2016-03-01 13:34:08 -0600

    add run command to deploy script

--- deploy.sh
+++ deploy.sh
@@ -5,3 +5,6 @@ function log () { echo "[+] $1"; };
 # install each local module
 log "Installing all local modules, hopefully"
 npm install --localapp file:lib/database file:lib/riot file:lib/update-champs file:lib/webserver
+
+# run
+node index.js

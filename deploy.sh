9b043fc33422e470c05aec5ef6bc80a97f27a029
ChaoticWeg <shawn.lutch94@gmail.com>
2016-03-01 13:30:57 -0600

    add procfile and deploy script

---
+++ deploy.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+function log () { echo "[+] $1"; };
+
+# install each local module
+log "Installing all local modules, hopefully"
+npm install --localapp file:lib/database file:lib/riot file:lib/update-champs file:lib/webserver

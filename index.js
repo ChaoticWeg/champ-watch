f2bd550d8935f05ad367d5f9265dbcad1b37ed50
ChaoticWeg <shawn.lutch94@gmail.com>
2016-03-01 13:35:59 -0600

    update database once in a while, eh?

--- index.js
+++ index.js
@@ -4,5 +4,5 @@ var updater = require('./lib/update-champs');
 webapp.listen(webapp.get('port'), function () {
 	console.log('listening on port %d', webapp.get('port'));
 	console.log('starting updater...');
-	// updater.start();
+	updater.start();
 });

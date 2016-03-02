792367003d673447def5ff2b81093cc3c5c279bc
ChaoticWeg <shawn.lutch94@gmail.com>
2016-03-02 09:43:19 -0600

    update database before webserver starts
    working on issue #1

--- index.js
+++ index.js
@@ -3,10 +3,19 @@ var riot     = require('./lib/riot');
 var webapp   = require('./lib/webserver');
 var updater  = require('./lib/update-champs');
 
+var start = function () {
+	webapp.listen(webapp.get('port'), function () {
+		console.log('listening on port %d', webapp.get('port'));
+		console.log('starting updater...');
 
-webapp.listen(webapp.get('port'), function () {
-	console.log('listening on port %d', webapp.get('port'));
-	console.log('starting updater...');
+		updater.start();
+	});
+};
 
-	updater.start();
-});
+var error = function (err) {
+	if (err) throw err;
+};
+
+var Promise    = require('promise');
+var fullUpdate = updater.promise.fullUpdate;
+fullUpdate.getPromise(start, error);

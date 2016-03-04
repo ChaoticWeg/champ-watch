04cf3351608b329740d24e21f852ddca38073506
ChaoticWeg <shawn.lutch94@gmail.com>
2016-03-03 22:48:33 -0600

    fetch datadragon version at startup
    fix #11 .. fetch the datadragon version automatically. TODO: is there
    a different, better way?

--- index.js
+++ index.js
@@ -3,19 +3,28 @@ var riot     = require('./lib/riot');
 var webapp   = require('./lib/webserver');
 var updater  = require('./lib/update-champs');
 
+// called whenever full update succeeds
 var start = function () {
-	webapp.listen(webapp.get('port'), function () {
-		console.log('listening on port %d', webapp.get('port'));
-		console.log('starting updater...');
+	// get datadragon version from riot
+	riot.getDataDragonVersion(function (version) {
+		process.env.DDRAGON_VERSION = version;
+		console.log('DataDragon version %s', process.env.DDRAGON_VERSION);
 
-		updater.start();
+		// start the webapp, and then start the updater
+		webapp.listen(webapp.get('port'), function () {
+			console.log('listening on port %d', webapp.get('port'));
+
+			console.log('starting updater...');
+			updater.start();
+		});
 	});
 };
 
+// called whenever full update fails
+// TODO: handle gracefully
 var error = function (err) {
 	if (err) throw err;
 };
 
-var Promise    = require('promise');
 var fullUpdate = updater.promise.fullUpdate;
 fullUpdate.getPromise(start, error);
